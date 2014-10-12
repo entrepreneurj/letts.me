233c0ff83070f7102c43094e6bf9cba86896dbde
Joe <entrepreneurj@gmail.com>
2014-10-12 00:14:56 +0000

    Small mistake

--- letts_me/blog/models.py
+++ letts_me/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+# Blog Entry
 class Entry(models.Model):
 
     title = models.CharField(max_length=100)
--- letts_me/letts_me/urls.py
+++ letts_me/letts_me/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
      url(r'links/(?P<page>[0-9]+)/$', LinksIndexView.as_view(), name='links-index'),
      url(r'blog/$', RedirectView.as_view(url=reverse_lazy('blog-index', args=[1])) ),
      url(r'blog/(?P<page>[0-9]+)/$', BlogListView.as_view(), name='blog-index'),
-     url(r'blog/(?P<slug>[a-zA-Z0-9-]+)/$', BlogDetailView.as_view(), name='blog-entry'),
+     url(r'blog/(?P<slug>[a-zA-Z0-9-_]+)/$', BlogDetailView.as_view(), name='blog-entry'),
      #url(r'^$', 'letts_me.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
