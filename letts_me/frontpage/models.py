b2bb2824a004c44f039db59c85db141108c03738
Joe <entrepreneurj@gmail.com>
2014-10-30 04:17:41 +0000

    Adds gallery to project

--- letts_me/frontpage/models.py
+++ letts_me/frontpage/models.py
@@ -18,21 +18,29 @@ class HasCategory(models.Model):
 
     categories = models.ManyToManyField(Category, blank = True, null = True)
 
-
     class Meta:
         abstract = True
-
-
 
 
 class Gallery(models.Model):
 
     title = models.CharField(max_length = 50)
-    
+    publish_date = models.DateTimeField(auto_now_add = True)
+
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name_plural = "galleries"
+
+class HasGallery(models.Model):
+
+    gallery = models.ForeignKey(Gallery, blank = True, null = True)
+
+    class Meta:
+        abstract = True
 
 def upload_path(instance, filename):
+
     """ 
     upload_path(object, string) -> string
     Generates filepath & filename for new images
