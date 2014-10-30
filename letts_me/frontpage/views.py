d486af5f577fe773f8b17f8ea4083ff5bc440ec6
Joe <entrepreneurj@gmail.com>
2014-10-30 06:44:47 +0000

    Adds gallery detail page and make sneaky changes to css for galleries

--- letts_me/frontpage/views.py
+++ letts_me/frontpage/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 from links.models import Link
 from blog.models import Entry
 
+from .models import Gallery 
 class IndexView(TemplateView):
 
     template_name = "frontpage/index.html"
@@ -15,3 +16,7 @@ class IndexView(TemplateView):
         context["blog"] = Entry.published.latest()
         context["link"] = Link.link_manager.latest()
         return context
+
+class GalleryDetailView(DetailView):
+   
+   model = Gallery
