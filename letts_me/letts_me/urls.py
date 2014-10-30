43c108c90ad3df11ae8bdaa012708dd5994bf980
Joe <entrepreneurj@gmail.com>
2014-10-30 04:48:25 +0000

    Adds setting to serve media files during development

--- letts_me/letts_me/urls.py
+++ letts_me/letts_me/urls.py
@@ -27,4 +27,8 @@ urlpatterns = patterns('',
      #url(r'^$', 'letts_me.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+) 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
